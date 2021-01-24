d0dd68090c5b9a2e3461e8833879e1515f465ac4
artqxh <artqxh@gmail.com>
2021-01-24 17:39:52 +0100

    moving forward

--- diode_move.py
+++ diode_move.py
@@ -25,10 +25,6 @@ b1 = A[3,1]
 c1 = A[3,2]
 d1 = A[3,3]
 
-print(A)
-
-number, move = input("Enter a number of checker and move(l/r): ").split()
-
 
 B = np.array([['a4', 'b4', 'c4', 'd4'],
               ['a3', 'b3', 'c3', 'd3'],
@@ -36,29 +32,35 @@ B = np.array([['a4', 'b4', 'c4', 'd4'],
               ['a1', 'b1', 'c1', 'd1']
               ])
 
+print(A)
 
+while A[0,0] == 0 and A[0,1] == 0 and A[0,2] == 0 and A[0,3] == 0:
+    number, move = input("Enter a number of checker and move(l/r): ").split()
+    for i, x in enumerate(B):
+        for j, y in enumerate(x):
+            if number in y:
+                if move == 'l':
+                    if j > 0:
+                        A[i, j] = 0
+                        i = i - 1
+                        j = j - 1
+                        A[i, j] = 1
+                        print(A)
 
-for i, x in enumerate(B):
-    for j, y in enumerate(x):
-        if number in y:
-            if move == 'l':
-                if j > 0:
-                    A[i, j] = 0
-                    i = i - 1
-                    j = j - 1
-                    A[i, j] = 1
-                    print(A)
+                    else:
+                        print('Wrong direction')
+
+                elif move == 'r':
+                    if j < 3:
+                        A[i, j] = 0
+                        i = i - 1
+                        j = j + 1
+                        A[i, j] = 1
+                        print(A)
+
+                    else:
+                        print('Wrong direction')
+
                 else:
                     print('Wrong direction')
-            elif move == 'r':
-                if j < 3:
-                    A[i, j] = 0
-                    i = i - 1
-                    j = j + 1
-                    A[i, j] = 1
-                    print(A)
-                else:
-                    print('Wrong direction')
-            else:
-                print('Wrong direction')
 
